066d10695dd2c328eedee00f9def87bc70e057c2
Your Name <you@example.com>
2022-02-17 18:43:04 +0530

    scss

--- sample/src/App.vue
+++ sample/src/App.vue
@@ -4,7 +4,8 @@
   <router-view/>
 </template>
 
-<style>
+<style lang='scss'>
+@import "/assets/styles/style.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
